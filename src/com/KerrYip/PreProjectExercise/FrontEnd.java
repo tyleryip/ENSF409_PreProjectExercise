8587e9d6091f4044f9342ae763b53e082484a720
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-03-31 16:56:44 -0600

    added a GUI class

--- src/com/KerrYip/PreProjectExercise/FrontEnd.java
+++ src/com/KerrYip/PreProjectExercise/FrontEnd.java
@@ -1,11 +1,13 @@
 package com.KerrYip.PreProjectExercise;
 
 public class FrontEnd {
-
 	
 	public static void main(String [] args) {
 		System.out.println("Hello world");
 		System.out.println("I need Kyra");
+		
+		GUI gui = new GUI("Main Window");
+		
 	}
 	
 	

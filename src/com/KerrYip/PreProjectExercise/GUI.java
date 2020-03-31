76f73152ddf47a0d71b0fdadd7c62b03226652cb
kerrmwill <kerrmwill@gmail.com>
2020-03-31 17:05:29 -0600

    Merge branch 'master' of https://github.com/yipster8888/ENSF409_Final_Project
    # Conflicts:
    #	src/com/KerrYip/PreProjectExercise/FrontEnd.java

---
+++ src/com/KerrYip/PreProjectExercise/GUI.java
@@ -0,0 +1,17 @@
+package com.KerrYip.PreProjectExercise;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class GUI extends JFrame{
+
+	public GUI(String s) {
+		super(s);
+		
+		setLayout(new BorderLayout());
+		
+		setVisible(true);
+	}
+	
+}

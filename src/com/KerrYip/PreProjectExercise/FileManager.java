a3488706795272ca24a01747b98d65a4ea79472d
kerrmwill <kerrmwill@gmail.com>
2020-04-01 17:47:07 -0600

    Added feature to create tree from file in the GUI

--- src/com/KerrYip/PreProjectExercise/FileManager.java
+++ src/com/KerrYip/PreProjectExercise/FileManager.java
@@ -4,30 +4,32 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * Helper class for the FrontEnd to read input from files
+ *  @author tyleryip
+ *  @author kerrmwill
+ */
 public class FileManager{
 
-    public String getFilename(){
-        Scanner filenameScanner = new Scanner(System.in);
-        System.out.println("Enter the input filename: ");
-        String filename = filenameScanner.nextLine();
-        return filename;
-    }
-
-    public String[] readFromScanner(String filename){
+    /**
+     * Reads class list from a text file and returns the classes in a String array
+     * @param filename name of the file being read from for the class list
+     * @return Returns the class list in a String array with each class being String being a class
+     */
+    public String[] readFromFile(String filename){
         String line = "";
         try {
             File file = new File(filename);
             Scanner scanner = new Scanner(file);
             while (scanner.hasNextLine()) {
-                line += scanner.nextLine().trim() + " ";
-                line += "\0";
+                line += scanner.nextLine().trim() + " \0"; //adds each class to a single string separated by \0
             }
             scanner.close();
 
         }catch(FileNotFoundException e){
             e.printStackTrace();
         }
-        return line.split("\0");
+        return line.split("\0"); //returns a string array separating at all the \0 added in the while loop above
     }
 
 }

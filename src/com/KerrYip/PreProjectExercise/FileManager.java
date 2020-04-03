a6788b5ca4467205958bd96b4a0eca3f0d8bd582
Tyler Yip <tyler.yip1@ucalgary.ca>
2020-04-02 21:28:59 -0600

    formatting and removed insertview

--- src/com/KerrYip/PreProjectExercise/FileManager.java
+++ src/com/KerrYip/PreProjectExercise/FileManager.java
@@ -6,26 +6,29 @@ import java.util.Scanner;
 
 /**
  * Helper class for the FrontEnd to read input from files
- *  @author tyleryip
- *  @author kerrmwill
+ * 
+ * @author tyleryip
+ * @author kerrmwill
  */
-public class FileManager{
+public class FileManager {
 
-    /**
-     * Reads class list from a text file and returns the classes in a String array
-     * @param filename name of the file being read from for the class list
-     * @return Returns the class list in a String array with each class being String being a class
-     */
-    public String[] readFromFile(String filename) throws FileNotFoundException{
-        String line = "";
-            File file = new File(filename);
-            Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()) {
-                line += scanner.nextLine().trim() + " \0"; //adds each class to a single string separated by \0
-            }
-            scanner.close();
+	/**
+	 * Reads class list from a text file and returns the classes in a String array
+	 * 
+	 * @param filename name of the file being read from for the class list
+	 * @return Returns the class list in a String array with each class being String
+	 *         being a class
+	 */
+	public String[] readFromFile(String filename) throws FileNotFoundException {
+		String line = "";
+		File file = new File(filename);
+		Scanner scanner = new Scanner(file);
+		while (scanner.hasNextLine()) {
+			line += scanner.nextLine().trim() + " \0"; // adds each class to a single string separated by \0
+		}
+		scanner.close();
 
-        return line.split("\0"); //returns a string array separating at all the \0 added in the while loop above
-    }
+		return line.split("\0"); // returns a string array separating at all the \0 added in the while loop above
+	}
 
 }
--- src/com/KerrYip/PreProjectExercise/MainView.java
+++ src/com/KerrYip/PreProjectExercise/MainView.java
@@ -1,21 +1,29 @@
 package com.KerrYip.PreProjectExercise;
 
-import java.awt.*;
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 
 /**
  * This code represents the main view of the Application
+ * 
  * @author tyleryip
  * @author kerrmwill
  *
  */
 @SuppressWarnings("serial")
-public class MainView extends JFrame{
+public class MainView extends JFrame {
 
 	private BinSearchTree classTree;
 	private JButton b1, b2, b3, b4;
@@ -25,14 +33,14 @@ public class MainView extends JFrame{
 	public MainView(String s, int width, int height) {
 		super(s);
 		this.setSize(width, height);
-		
+
 		classTree = new BinSearchTree();
-	
+
 		b1 = new JButton("Insert");
 		b2 = new JButton("Find");
 		b3 = new JButton("Browse");
 		b4 = new JButton("Create Tree From File");
-		
+
 		b1.addActionListener((ActionEvent e) -> {
 			JPanel insertPanel = new JPanel();
 			insertPanel.setLayout(new BoxLayout(insertPanel, BoxLayout.PAGE_AXIS));
@@ -53,86 +61,87 @@ public class MainView extends JFrame{
 			inputPanel2.add(new JLabel("Enter Student's Major"));
 			JTextField majorLabel = new JTextField(15);
 			inputPanel2.add(majorLabel);
-			inputPanel2.add("South",new JLabel("Enter year"));
+			inputPanel2.add("South", new JLabel("Enter year"));
 			JTextField yearLabel = new JTextField(5);
-			inputPanel2.add("South",yearLabel);
+			inputPanel2.add("South", yearLabel);
 			insertPanel.add(inputPanel2);
 
-			Object[] options = {"Insert", "Return to Main Window"};
+			Object[] options = { "Insert", "Return to Main Window" };
 
-			int result =  JOptionPane.showOptionDialog(null, insertPanel, "Insert a new Node",
+			int result = JOptionPane.showOptionDialog(null, insertPanel, "Insert a new Node",
 					JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, null);
 
-			if(result == JOptionPane.OK_OPTION){
-				classTree.insert(studentIDLabel.getText(), facultyLabel.getText(), majorLabel.getText(), yearLabel.getText());
+			if (result == JOptionPane.OK_OPTION) {
+				classTree.insert(studentIDLabel.getText(), facultyLabel.getText(), majorLabel.getText(),
+						yearLabel.getText());
 			}
 		});
-		
+
 		b2.addActionListener((ActionEvent e) -> {
 			String studentID = JOptionPane.showInputDialog("Please enter the student's id");
-			Node result = classTree.find(classTree.root,studentID);
-			if(result == null){
-				JOptionPane.showMessageDialog(null,"Target record was not found");
-			}else{
-				JOptionPane.showMessageDialog(null,result.toString());
+			Node result = classTree.find(classTree.root, studentID);
+			if (result == null) {
+				JOptionPane.showMessageDialog(null, "Target record was not found");
+			} else {
+				JOptionPane.showMessageDialog(null, result.toString());
 			}
 		});
-		
+
 		b3.addActionListener((ActionEvent e) -> {
-			if(classTree.root != null) {
+			if (classTree.root != null) {
 				dataText.setText(classTree.toString());
 			}
 		});
-		
+
 		b4.addActionListener((ActionEvent e) -> {
 			String filename = JOptionPane.showInputDialog("Enter the file name:");
-			if(filename != null) {
+			if (filename != null) {
 				try {
 					FileManager fm = new FileManager();
 					classTree.insertList(fm.readFromFile(filename));
-				}catch(FileNotFoundException ex){
-					JOptionPane.showMessageDialog(null,"File not found.");
+				} catch (FileNotFoundException ex) {
+					JOptionPane.showMessageDialog(null, "File not found.");
 				}
 			}
 		});
-		
+
 		titleLabel = new JLabel();
 		titleLabel.setText("An Application to Maintain Student Records");
-		
-		dataText = new JTextArea(height/25,width/15);
-		dataText.setLineWrap(true); //Allows text to wrap if it reaches the end of the line
-		dataText.setWrapStyleWord(true); //text should wrap at word boundaries rather than character boundaries
+
+		dataText = new JTextArea(height / 25, width / 15);
+		dataText.setLineWrap(true); // Allows text to wrap if it reaches the end of the line
+		dataText.setWrapStyleWord(true); // text should wrap at word boundaries rather than character boundaries
 		dataText.setEditable(false);
-		dataText.setText(""); //This displays empty text in the field
+		dataText.setText(""); // This displays empty text in the field
 		dataText.setEditable(false);
-		
+
 		JScrollPane dataTextScrollPane = new JScrollPane(dataText);
 		dataTextScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		
-		//The following code sets up the panels and layout
+
+		// The following code sets up the panels and layout
 		JPanel buttonPanel = new JPanel();
 		JPanel titlePanel = new JPanel();
 		JPanel dataPanel = new JPanel();
 
 		setLayout(new BorderLayout());
-		
+
 		buttonPanel.add(b1);
 		buttonPanel.add(b2);
 		buttonPanel.add(b3);
 		buttonPanel.add(b4);
-		
+
 		titlePanel.add(titleLabel);
 		dataPanel.add(dataTextScrollPane);
-		
+
 		add("North", titlePanel);
 		add("Center", dataPanel);
 		add("South", buttonPanel);
-		
+
 		dataTextScrollPane.setBorder(dataPanel.getBorder());
-		
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//pack();
+		// pack();
 		setVisible(true);
 	}
-	
+
 }
